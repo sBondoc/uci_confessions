64212179d2e7a89fde6d41cb92087ab5ea8b578b
Sam <42726060+sBondoc@users.noreply.github.com>
2019-02-04 02:11:28 -0800

    Create get_title.php

---
+++ php/get_title.php
@@ -0,0 +1,14 @@
+<?php
+
+function get_title($url){
+  $str = file_get_contents($url);
+  if(strlen($str)>0){
+    $str = trim(preg_replace('/\s+/', ' ', $str)); // supports line breaks inside <title>
+    preg_match("/\<title\>(.*)\<\/title\>/i",$str,$title); // ignore case
+    return $title[1];
+  }
+}
+//Example:
+echo get_title("http://www.washingtontimes.com/");
+
+?>
