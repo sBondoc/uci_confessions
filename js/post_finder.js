72f181a5f7d963823b92a9c7f91a8c6d13179cb5
Sam <42726060+sBondoc@users.noreply.github.com>
2019-02-04 01:01:32 -0800

    Update post_finder.js

--- js/post_finder.js
+++ js/post_finder.js
@@ -1,16 +1,13 @@
-function setText(text) {
-      document.getElementById("testText").innerHTML = text;
-}
-
-function getTitle(urlText) {
-      var title = "nope";
-      
-      jQuery.ajax({
-            url: "https://www.facebook.com/Confessions.UCI/posts/296931287675788",
-            complete: function(data) {
-                  console.log(data.responseText);
-            }
-      });
-      
-      return title;
+function getTitle(externalUrl){
+  var proxyurl = "http://localhost/get_external_content.php?url=" + externalUrl;
+  $.ajax({
+    url: proxyurl,
+    async: true,
+    success: function(response) {
+      alert(response);
+    },   
+    error: function(e) {
+      alert("error! " + e);
+    }
+  });
 }
